7f976f69082a8208afed75c1a009feb01cc40c41
Sophia <soph4@umbc.edu>
2019-02-16 01:37:39 -0500

    Loads word file and selects 25 random words from the list

--- codeNames.py
+++ codeNames.py
@@ -1,1 +1,45 @@
-#File: codeNames.py
+# File: codeNames.py
+# Description:
+
+
+NUM_CARDS = 25
+
+import random
+
+# loads the words from a given text file
+def loadWords():
+
+	fileName = input("Please enter word list: ")
+	text = open(fileName, "r")
+
+	words = []
+
+	for line in text:
+		line = line.strip()
+		words.append(line)
+
+	return words
+
+# prints the give list
+def printList(wordList):
+
+	for i in range(len(wordList)):
+		print(wordList[i])	
+	
+
+# randomly selects 25 words for the game
+def selectWords(wordList):
+	
+	chosenWords = []
+	randNums = random.sample(range(len(wordList)), NUM_CARDS)
+	for i in range(len(randNums)):
+		chosenWords.append(wordList[randNums[i]])
+
+	return chosenWords
+
+def main():
+	wordList = loadWords()
+	#printList(wordList)
+	printList(selectWords(wordList))
+
+main()
