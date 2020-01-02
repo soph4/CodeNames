cfbdf934b2e98945b03b59fa889134b3146710d4
Sophia Hu <sophiahu@Sophias-MBP.home>
2020-01-01 21:41:21 -0500

    Add timer Change organization of word buttons

--- gui.py
+++ gui.py
@@ -1,78 +1,78 @@
 from tkinter import *
 import tkinter as tk
-import random
 import codeNames
 from PIL import ImageTk, Image
 
-NUM_CARDS = 25
-
 class App(tk.Frame):
+    # contains all the possible words
     wordList = []
+    # 25 randomly selected words for the game 
     chosenWords = []
+    texts=[]
 
     def __init__(self, master=None):
         super().__init__(master)
         self.pack()
+        App.x = IntVar()
+        #App.text = StringVar()
         self.wordList = codeNames.loadWords()
-        #self.loadWords()
-        self.selectWords()
+        self.chosenWords = codeNames.selectWords(self.wordList)
         self.createButtons()
-        #self.create_widgets()
-        #self.display_image()
-    
-    def update_text(string):
-        print("String: ", string)
+        self.setTimer()
+        self.setTeamCards()
 
-    def loadWords(self):
-        #fileName = input("Please enter word list: ")
-        #if(fileName == ""):
-        fileName = "wordlist2.txt"
-        text = open(fileName, "r")
-        
-        words = []
-        for line in text:
-            line = line.strip()
-            words.append(line)
-        self.wordList = words
+    def wordClicked(self, index):
+        print(index)
+        self.texts[index].set(self.chosenWords[index].typeOfCard)
 
-    def selectWords(self):
-        chosen = []
-        randNums = random.sample(range(len(self.wordList)), NUM_CARDS)
-   
-        index = 0
-        while index < NUM_CARDS:
-            chosen.append(self.wordList[randNums[index]].lower())
-            index += 1
-        random.shuffle(chosen)
-        self.chosenWords = chosen
-
+    # creates 5 by 5 grid of board of words
     def createButtons(self):
-        index = 0
+        index= 0
+        center = Frame(self)
         for i in range(5):
-            row = Frame(self)
+            row = Frame(center)
             for j in range(5):
-                word = self.chosenWords[index]
-                tk.Button(row, text = word ,command=lambda: update_text(1)).pack(padx=5, pady=10, side=tk.BOTTOM)
+                self.texts.append(StringVar())
+                self.texts[index].set(self.chosenWords[index].word)
+                tk.Button(row, textvariable = self.texts[index] , font = ("",18), command= lambda: self.wordClicked(index)).pack(ipadx=10, padx=5, pady=10, side=tk.BOTTOM)
                 index += 1
-            row.pack(side = LEFT)
+            row.pack(ipadx=5, side = LEFT)
+
+        center.pack(side = BOTTOM)
+
+    def setTeamCards(self):
+        bottom = Frame(self)
+        part = Frame(bottom)
+        tk.Label(part, text = "RED TEAM cards left: ", font = ("",18)).pack(side=LEFT)
+        tk.Label(part, text = "BLUE TEAM cards left: ", font = ("",18)).pack(side=RIGHT)
+        part.pack(side = BOTTOM)
+        bottom.pack(side = BOTTOM)
 
 
-    def create_widgets(self):
-        #string = "bye"
-        self.hello = tk.Button(self, text="hello", command=lambda: update_text(1)).pack()
-        #self.hello["text"] = "Welcome to Code Names\n(click me)"
-        #self.hello["command"] = self.welcome()
-        #self.hello.pack(side="top")
-        
-        self.quit = tk.Button(self, text="QUIT",fg="red",command=self.master.destroy)
-        self.quit.pack(side="bottom")
-    
-    def welcome(self):
-        print("Hello welcome to the game!")
-    
-    #def update_text(string):
-    #    print("String:", string)
+    # starts the timer and counts down to zero
+    def startTimer(self):
+        self.x.set(self.x.get() - 1)   
+        if self.x.get() > 0:
+            self.after(1000, self.startTimer)
+        else:
+            self.restartTimer()
 
+    # resets the timer back to one minute 
+    def restartTimer(self):
+        self.x.set(60)
+
+    # creates and sets the timer for the game
+    def setTimer(self):
+        top = Frame(self)
+        time = Frame(top)
+        tk.Button(time, text = "PRESS TO START", font = ("",18), command = self.startTimer).pack(side = LEFT)
+        tk.Label(time, text = "TIMER:", font = ("",18)).pack(side = LEFT)
+        self.x.set(60)
+        tk.Label(time, textvariable = self.x, font = ("", 18)).pack(side = RIGHT)
+        time.pack(side = LEFT)
+        top.pack(side = TOP)
+
+    # extra function to display images 
     def display_image(self):
         image = Image.open("cute_dog.jpg")
         image = image.resize((190,250), Image.ANTIALIAS)
@@ -84,6 +84,6 @@ class App(tk.Frame):
 root = tk.Tk()
 app = App(master=root)
 app.master.title("Code Names")
-app.master.maxsize(1000,4000)
+app.master.maxsize(1500,50000)
 app.mainloop()
 
